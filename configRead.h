7ed93f7b25e1c19cecea636a686aa7a32874b034
3038922 <31612534@qq.com>
2019-01-19 20:06:35 +0800

    新增一个CONFIG

---
+++ configRead.h
@@ -0,0 +1,164 @@
+#pragma once
+#include <algorithm>
+#include <exception>
+#include <fstream>
+#include <iostream>
+#include <unordered_map>
+#include <sstream>
+#include <string>
+#include <type_traits>
+
+std::string cfg_path{ "config.txt" };
+
+//  Helper函数，如果字符串读取，则将字符串转换为bool
+//  true它返回true，否则返回false
+bool to_bool(std::string str) {
+	std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+	if (str.compare("true") == 0) return true;
+	else if (str.compare("false") == 0) return false;
+	else throw std::invalid_argument("Boolean can only be false or true, impartial to case.");
+}
+
+class ConfigReader
+{
+	// Default Constructor构建内部数据结构
+	ConfigReader()
+	{
+		if (!parse_file()) throw ConfigReaderException();
+	}
+
+	// /在以下情况下使用的异常：
+// /  - 构造函数失败
+	class ConfigReaderException : public std::exception
+	{
+		virtual const char* what() const throw()
+		{
+			return "A ConfigReaderException happened";
+		}
+	};
+
+	//如果发生不可更改的错误，请尝试解析配置文件
+	//我们返回fasel，如果该行是格式错误的跳过行，否则
+	// 返回true
+	bool parse_file()
+	{
+		size_t curr_line = 0;
+
+		std::fstream cfg_file{ cfg_path };
+		if (cfg_file.fail()) return false;
+
+		std::string line;
+		while (std::getline(cfg_file, line))
+		{
+			std::string key, val;
+			size_t del_idx = line.find('=');
+
+			//我们每行只能有一个=
+			if (line.find('=', del_idx + 1) != std::string::npos)
+			{
+				std::cerr << "More than one '=' on line " << curr_line << std::endl;
+				std::cerr << "Skipping line..." << curr_line + 1 << std::endl;
+				curr_line++;
+				continue;
+			}
+
+			//解析密钥和值
+			key = line.substr(0, del_idx);
+			val = line.substr(del_idx + 1, line.length());
+
+			//插入容器
+			m_cfg_data[key] = val;
+
+			curr_line++;
+		}
+
+		return true;
+	}
+
+	static ConfigReader* s_instance;
+
+	// / Datastruct在内部保存配置文件
+	std::unordered_map<std::string, std::string> m_cfg_data;
+
+public:
+
+	//返回与key对应的值作为std :: string
+	std::string get(std::string key)
+	{
+		auto search = m_cfg_data.find(key);
+
+		if (search == m_cfg_data.end())
+		{
+			std::cerr << "Can't find key: " << key << std::endl;
+			return "";
+		}
+
+		return search->second;
+	}
+
+	/**
+	* SFINAE用于补偿解析原始数据类型。
+	*如果请求复杂数据类型，我们假设一个构造函数
+	形式为foo（std::string）的*可用。
+	*/
+	// / INTEGRAL TYPES
+	template<class T, typename std::enable_if<std::is_integral<T>::value && !std::is_same<bool, T>::value>::type* = nullptr>
+	T get_as(std::string key)
+	{
+		auto found_val = this->get(key);
+		T ret_val = 0;
+		try
+		{
+			ret_val = std::stoi(found_val);
+		}
+		catch (const std::invalid_argument& ia)
+		{
+			std::cerr << "Key: " << key << " throws Invalid Argument" << std::endl;
+			std::cerr << ia.what() << std::endl;
+			return T();
+		}
+		return ret_val;
+	}
+
+	// /浮动类型
+	template<class T, typename std::enable_if<std::is_floating_point<T>::value>::type* = nullptr>
+	T get_as(std::string key)
+	{
+		auto found_val = this->get(key);
+		T ret_val = 0.0f;
+		try
+		{
+			ret_val = std::stod(found_val);
+		}
+		catch (const std::invalid_argument& ia)
+		{
+			std::cerr << "Key: " << key << " throws Invalid Argument" << std::endl;
+			std::cerr << ia.what() << std::endl;
+			return T();
+		}
+		return ret_val;
+	}
+
+	// / BOOL
+	template<class T, typename std::enable_if<std::is_same<bool, T>::value>::type* = nullptr>
+	T get_as(std::string key)
+	{
+		auto found_val = this->get(key);
+		T ret_val = to_bool(found_val);
+		return ret_val;
+	}
+	// / COMPLEX TYPE
+	template<class T, typename std::enable_if<std::is_class<T>::value>::type* = nullptr>
+	T get_as(std::string key)
+	{
+		auto found_val = this->get(key);
+		return T(found_val);
+	}
+
+	// /实现Singleton模式
+	static ConfigReader* instance()
+	{
+		if (ConfigReader::s_instance == nullptr) ConfigReader::s_instance = new ConfigReader();
+		return s_instance;
+	}
+};
--- opcontrol.cpp
+++ opcontrol.cpp
@@ -1,38 +1,42 @@
 #include"main.h"
 #include "poten.h"
 #include <json.h>
-
+#include <fstream>
+#define MAX_BUF_LEN 1024
 void opcontrol()
 {
-	Json::Value root;   // 'root' will contain the root value after parsing.
-	std::cin >> root;
-	Json::Reader reader;
-	// You can also read into a particular sub-value.
-	std::cin >> root["subtree"];
-	// Get the value of the member of root named 'encoding',
-	// and return 'UTF-8' if there is no such member.
-	std::string encoding = root.get("encoding", "UTF-8").asString();
-	// Get the value of the member of root named 'plug-ins'; return a 'null' value if
-	// there is no such member.
-	const Json::Value plugins = root["plug-ins"];
-	// Iterate over the sequence elements.
-	//for (int index = 0; index < plugins.size(); ++index)
-	//	loadPlugIn(plugins[index].asString());
+	FILE* file = fopen("example.json", "r");
+	char buf[MAX_BUF_LEN];
+	while (fgets(buf, MAX_BUF_LEN, file) != nullptr)//读取一行
+	{
+		std::string temp = buf;
+		std::cout << temp ;
+	}
 
-	//// Try other datatypes. Some are auto-convertible to others.
-	//foo::setIndentLength(root["indent"].get("length", 3).asInt());
-	//foo::setIndentUseSpace(root["indent"].get("use_space", true).asBool());
-	//// Since Json::Value has an implicit constructor for all value types, it is not
-	//// necessary to explicitly construct the Json::Value object.
-	//root["encoding"] = foo::getCurrentEncoding();
-	//root["indent"]["length"] = foo::getCurrentIndentLength();
-	//root["indent"]["use_space"] = foo::getCurrentIndentUseSpace();
-	// If you like the defaults, you can insert directly into a stream.
-	std::cout << root;
-	// Of course, you can write to `std::ostringstream` if you prefer.
-	// If desired, remember to add a linefeed and flush.
-	std::cout << std::endl;
+	//Json::Value root;
+	//Json::Reader reader;
+	//std::string test = "{\"name\" : \
+	//						\"Tocy\", \"salary\" : 100, \"msg\" : \"work hard\", \
+ //       \"files\" : [\"1.ts\", \"2.txt\"]}";
+	//std::vector<std::pair<std::string, std::string>> test2 = ;
+	//if (!reader.parse(test2, root)) {
+	//	// "parse fail";
+	//}
+	//else
+	//	std::cout << root["王八蛋"].asString();//print "UP000000"
 
+	//Json::Value root;
+	//Json::Reader reader;
+	//std::ifstream ifs("example.json");//open file example.json
+
+	//if (!reader.parse(ifs, root)) {
+	//	// fail to parse
+	//}
+	//else {
+	//	// success
+	//	std::cout << root["你好"].asString() << std::endl;
+	//	std::cout << root["indent"]["length"].asInt() << std::endl;
+	//}
 
 	//ncrapi::Poten poten(1, 5);
 	//std::fstream fs("Text1.txt");
