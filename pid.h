9afe7e2d28c5436d5a9c93c6b50c5fdb6e5e5528
3038922 <31612534@qq.com>
2019-01-28 23:42:11 +0800

    类 数据需要分离以便实时调参和遍历

--- pid.h
+++ pid.h
@@ -10,9 +10,9 @@ public:
 		for (auto& it : pragma[name])
 			for (auto& it1 : it)
 				_pragma.push_back(it1.asDouble());
-		std::cout << "PID基类构建完成,以下为参数:" << std::endl;
+	/*	std::cout << "PID基类构建完成,以下为参数:" << std::endl;
 		for (auto& it : _pragma)
-			std::cout << it << std::endl;
+			std::cout << it << std::endl;*/
 	}
 
 private:
