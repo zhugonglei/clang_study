2b6e5fb005f1536e004656dc55c38de191a73850
3038922 <31612534@qq.com>
2019-01-29 23:43:55 +0800

    彻底告别坑爹的VS2019
    Signed-off-by: 3038922 <31612534@qq.com>

--- src/init.cpp
+++ src/init.cpp
@@ -10,35 +10,40 @@ using json = nlohmann::json;
 // Generic* intake;
 // Generic* shoot;
 
-void init() {
-  std::cout << "你好世界" << std::endl;
-  //   json jsonData = {"撒旦法", 6, "暗示法的", 8};
-  //   std::cout << jsonData[0] << std::endl;
-  //��������е�һ��Ԫ��"a"��Ӧ�ļ�ֵ
-  // std::for_each(jsonData.begin(), jsonData.end(), [](auto it) {std::cout <<
-  // it << std::endl; });
+void init()
+{
+    // #if defined XXX
+    std::cout << "你好世界" << std::endl;
+    // #else
 
-  // Motor a(val["����"]["���"][3]);
-  // ADI gyro(val["����"]["ADI"][0]);
-  // on::Value::Members key = val["����"]["���"]["��ǰ"].getMemberNames();
-  // std::cout << val["����"]["���"].size() << std::endl;
-  // std::cout << val["Skills"][0].asString() << std::endl;
-  // size_t count = 0;
-  // for (auto& it : val["����"]["���"]["�Һ�"])
-  //{
-  //	auto key = val["����"]["���"]["�Һ�"].getMemberNames();
-  //	std::cout << key[count] << " : " << it.asInt() << std::endl;
-  //	count++;
-  //}
-  // for (auto &it:key) //����json��Ա
-  //{
-  //	std::string name = it;
-  //	int flag = val[it].asInt();
-  //	std::cout << name << ":" << flag << std::endl;
-  //}
-  // sysData = new SystemData(jsonData);
-  // chassis = new Chassis(sysData->jsonVal["����"]);
-  // lift = new Generic({ Motor{5,-1} }, "����");
-  // intake = new Generic({ Motor{6,-1} }, "����");
-  // shoot = new Generic({ Motor{7,-1}, Motor{8,1} }, "����");
+    // #endif
+    //   json jsonData = {"撒旦法", 6, "暗示法的", 8};
+    //   std::cout << jsonData[0] << std::endl;
+    //��������е�һ��Ԫ��"a"��Ӧ�ļ�ֵ
+    // std::for_each(jsonData.begin(), jsonData.end(), [](auto it) {std::cout <<
+    // it << std::endl; });
+
+    // Motor a(val["����"]["���"][3]);
+    // ADI gyro(val["����"]["ADI"][0]);
+    // on::Value::Members key = val["����"]["���"]["��ǰ"].getMemberNames();
+    // std::cout << val["����"]["���"].size() << std::endl;
+    // std::cout << val["Skills"][0].asString() << std::endl;
+    // size_t count = 0;
+    // for (auto& it : val["����"]["���"]["�Һ�"])
+    //{
+    //	auto key = val["����"]["���"]["�Һ�"].getMemberNames();
+    //	std::cout << key[count] << " : " << it.asInt() << std::endl;
+    //	count++;
+    //}
+    // for (auto &it:key) //����json��Ա
+    //{
+    //	std::string name = it;
+    //	int flag = val[it].asInt();
+    //	std::cout << name << ":" << flag << std::endl;
+    //}
+    // sysData = new SystemData(jsonData);
+    // chassis = new Chassis(sysData->jsonVal["����"]);
+    // lift = new Generic({ Motor{5,-1} }, "����");
+    // intake = new Generic({ Motor{6,-1} }, "����");
+    // shoot = new Generic({ Motor{7,-1}, Motor{8,1} }, "����");
 }
