34228bfb1a7d903ad9e6cbcae1f66e7cadee3285
3038922 <31612534@qq.com>
2019-01-30 12:30:21 +0800

    阅读文档研究如何get key
    Signed-off-by: 3038922 <31612534@qq.com>

--- src/init.cpp
+++ src/init.cpp
@@ -14,8 +14,14 @@
 void init()
 {
     json j1 = json::parse(userConfig);
+    //基于迭代器的rangFor
+    for (auto it = j1["底盘"]["马达"].begin(); it != j1["底盘"]["马达"].end(); it++)
+    {
+        std::cout << it.key() << it.value()["端口"] << std::endl;
+    }
+    // ;
     for (auto &it : j1["底盘"]["马达"])
-        std::cout << it << std::endl;
+        std::cout << "= " << it["端口"] << std::endl;
     // std::cout << j1["底盘"]["马达"]["左前"].get<std::string>() << std::endl;
 
     // json j2 = {
